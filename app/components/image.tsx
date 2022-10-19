9a87b7d75fd834559cdb40a407174500d0ab2ffc
Jack DeVries <jdevries3133@gmail.com>
2022-10-18 22:45:19 -0400

    additional iteration on git help post

--- app/components/image.tsx
+++ app/components/image.tsx
@@ -5,7 +5,7 @@ export const Image: React.FC<{
 }> = ({ src, alt, caption }) => {
   return (
     <div className="not-prose">
-      <img className="w-full" src={src} alt={alt} />
+      <img className="w-full rounded" src={src} alt={alt} />
       <span className="text-clay-800 text-sm">{caption}</span>
     </div>
   );
