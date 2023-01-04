3428d723a8d91a969640c8708454e13dd386c1a9
Jack DeVries <jdevries3133@gmail.com>
2023-01-03 22:34:26 -0500

    add calendy page

---
+++ app/routes/calendy.tsx
@@ -0,0 +1,21 @@
+import { DefaultPageContainer } from "~/components/pageContainer";
+
+export default function Calendy() {
+  return (
+    <DefaultPageContainer>
+      <div className="rounded p-2 bg-clay-100 prose">
+        <h1>Hi CTD Crew</h1>
+        <p>
+          My one and only free calendy link has been reallocated, may she rest
+          in peace.
+        </p>
+        <p>
+          To schedule a 1:1, just send me a slack, or email me at{" "}
+          <a href="mailto:jdevries3133@gmail.com">jdevries3133@gmail.com</a>. My
+          a slack sessions for CTD are still Tuesday 6pm-10pm EST. I will post
+          any availability updates here as necessary.
+        </p>
+      </div>
+    </DefaultPageContainer>
+  );
+}
