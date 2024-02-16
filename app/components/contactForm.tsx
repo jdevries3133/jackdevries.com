2a15dfe2b102b3f15ffe88f191664890acd8bcfc
Jack DeVries <jdevries3133@gmail.com>
2024-02-15 21:01:14 -0500

    fix: typescript error

--- app/components/contactForm.tsx
+++ app/components/contactForm.tsx
@@ -1,14 +1,7 @@
-import { PrimaryButton } from "./buttons";
-
 export const ContactForm = () => {
   return (
     <a href="mailto:jdevries3133@gmail.com">
-      <PrimaryButton
-        data-testid="contactMeFormOpener"
-        onClick={() => setShowForm(true)}
-      >
         Contact Me (jdevries3133@gmail.com)
-      </PrimaryButton>
     </a>
   );
 };
