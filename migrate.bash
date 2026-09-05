3146d5a60c2b4019e7263e9f18ab256b0f139685
Jack DeVries <jdevries3133@gmail.com>
2026-09-04 21:38:27 -0400

    feat(migrate): migrate script restarts at the beginning

--- migrate.bash
+++ migrate.bash
@@ -2,7 +2,14 @@
 
 set -euxo pipefail
 
+git checkout -- .
+git clean -df
+
 mkdir -p markdown
 mv app/mdx/*.mdx markdown
-ls | grep -v \.git | grep -v markdown | grep -v public | xargs rm -rf
+ls | grep -v migrate.bash \
+    | grep -v \.git \
+    | grep -v markdown \
+    | grep -v public \
+    | xargs rm -rf
 ls public | grep -v static | xargs rm -rf
