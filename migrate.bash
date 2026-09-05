08f57a54e7865da77fc6fa5ff0cecf4e20977616
Jack DeVries <jdevries3133@gmail.com>
2026-09-04 21:53:41 -0400

    feat(migrate): a wee bit of abstraction

--- migrate.bash
+++ migrate.bash
@@ -2,14 +2,25 @@
 
 set -euxo pipefail
 
-git checkout -- .
-git clean -df
+restore() {
+    git checkout -- .
+    git clean -df
+}
 
-mkdir -p markdown
-mv app/mdx/*.mdx markdown
-ls | grep -v migrate.bash \
-    | grep -v \.git \
-    | grep -v markdown \
-    | grep -v public \
-    | xargs rm -rf
-ls public | grep -v static | xargs rm -rf
+nuke_trash() {
+    mkdir -p markdown
+    mv app/mdx/*.mdx markdown
+    ls | grep -v migrate.bash \
+        | grep -v \.git \
+        | grep -v markdown \
+        | grep -v public \
+        | xargs rm -rf
+    ls public | grep -v static | xargs rm -rf
+}
+
+main() {
+    restore
+    nuke_trash
+}
+
+main
